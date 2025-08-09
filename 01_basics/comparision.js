5b94db41ff756d099dba513b44e462a18932592f
Aman Kumar <91582409+Aman1953@users.noreply.github.com>
2025-08-09 20:32:39 +0000

    dataTypes Summary = {   id: string;   title: string;   description: string;   createdAt: string;   updatedAt: string; };

--- 01_basics/comparision.js
+++ 01_basics/comparision.js
@@ -6,3 +6,14 @@ console.log(null >=0); // true (in comparision operator null is first converted 
 // ===   Strict Equality
 console.log("2"===2) // false
 
+console.log(typeof 2343434n); // bigint
+
+
+console.log(typeof null); // object (this is a known bug in JavaScript, null is not an object, but typeof null returns "object")
+console.log(typeof undefined); // undefined (this is the only type that returns itself)
+
+
+const id = Symbol("123");
+const id2 = Symbol("123");
+console.log(id === id2); // false (symbols are unique, even if they have the same description)
+console.log(typeof id); // symbol (this is a new primitive type in JavaScript, used for unique identifiers)
