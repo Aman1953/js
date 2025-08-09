2be5f0e677fee15478e86de011b398512e5ff285
Aman Kumar <91582409+Aman1953@users.noreply.github.com>
2025-08-09 20:18:14 +0000

    added operstions

---
+++ 01_basics/basicOperations.js
@@ -0,0 +1,16 @@
+// console.log(2**4) // 2 to the power 4
+let a = 5;
+let neg_a = -a;
+// console.log(neg_a); // -5
+
+console.log(1+"2");//12
+console.log("1"+2);//12
+console.log(1+2+"3");//33
+console.log("1"+2+3);//123
+
+
+
+
+
+
+
---
+++ 01_basics/comparision.js
@@ -0,0 +1,8 @@
+console.log(null>0); // false
+console.log(null<0); // false
+console.log(null == 0); // false (this is equality check, not comparision)
+console.log(null >=0); // true (in comparision operator null is first converted into 0)
+
+// ===   Strict Equality
+console.log("2"===2) // false
+
